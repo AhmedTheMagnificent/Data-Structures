3fa335e8e8bfe8738c0549e0d065db1a697a91b5
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-10-15 17:15:20 +0500

    added red black trees left rotations'

--- Trees/Red-Black-Trees.cpp
+++ Trees/Red-Black-Trees.cpp
@@ -14,6 +14,7 @@ struct node{
 class RedBlackTree{
 private:
     node *root;
+    node *NIL;
     node *insert(int val, node *root){
         if(root == nullptr){
             return new node(val);
@@ -22,20 +23,63 @@ private:
             if(val < root->value){
                 root->left = insert(val, root->left);
                 root->left->parent = root;
-            }
-            else{
+            } else{
                 root->right = insert(val, root->right);
                 root->right->parent = root;
             }
         }
-        if(root->parent == "red"){
-            
-        }
+        root = fixViolations(root);
         return root;
     }
 
+    node *fixViolations(node *root){
+        node *parent = nullptr;
+        node *grandparent = nullptr;
+        while(root->color == "RED" && root->parent->color == "RED"){
+            parent = root->parent;
+            grandparent = parent->parent;
+            if(parent == grandparent->right){
+                node *uncle = grandparent->left;
+
+                if(uncle != nullptr && uncle->color == "RED"){
+                    grandparent->color = "RED";
+                    parent->color = "BLACK";
+                    uncle->color = "BLACK";
+                    root = grandparent;
+                }
+
+                else if(root == parent->right && uncle->color == "BLACK"){
+                    
+                }
+            }
+            else{
+                node *uncle = grandparent->right;
+                if(uncle != nullptr && uncle->color == "RED"){
+                    grandparent->color = "RED";
+                    parent->color = "BLACK";
+                    uncle->color = "BLACK";
+                    root = grandparent;
+                }
+            }
+        }
+    }
+
+    void leftRotate(node *x){
+        node *y = x->right;
+        x->right = y->left;
+        if(y->left != NIL){
+            y->left->parent = x;
+        }
+        
+    }
+
 public:
+    RedBlackTree(void){
+        root = nullptr;
+    }
+
     void insert(int val){
         root = insert(val, root);
+        root->color = "BLACK";
     }
 };
